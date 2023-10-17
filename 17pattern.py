ce3dc76d8ab01a326a40e72f6dd1653ec33bca62
Ravi Kumawat <145572488+1998rv@users.noreply.github.com>
2023-10-17 14:36:21 +0530

    Add files via upload

---
+++ 17pattern.py
@@ -0,0 +1,63 @@
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print("*",end=" ")
+#     print("\r")
+
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print(6-i,end=" ")
+#     print("\r")
+
+# 8 8 8 8 8
+# 6 6 6 6
+# 4 4 4
+# 2 2
+# 0
+#
+# a = 8
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print(a,end=" ")
+#     a = a - 2
+#     print("\r")
+
+# E E E E E
+# D D D D
+# C C C
+# B B
+# A
+
+
+#
+# a = chr(69)
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print(a,end=" ")
+#     a = chr(69-i)
+#     print("\r")
+
+# 9 7 5 3 1
+# 7 5 3 1
+# 5 3 1
+# 3 1
+# 1
+
+# a = 10
+# for i in range(1,6):
+#     for j in reversed(range(1,a,2)):
+#         print(j,end=" ")
+#     a = a - 2
+#     print("\r")
+

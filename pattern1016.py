a941876fa6dda85575e05f7b2464a30d2a5cf2a1
Ravi Kumawat <145572488+1998rv@users.noreply.github.com>
2023-10-17 14:39:44 +0530

    Add files via upload

---
+++ pattern1016.py
@@ -0,0 +1,69 @@
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print(i,end=" ")
+#     print("\r")
+
+# 5
+# 4 4
+# 3 3 3
+# 2 2 2 2
+# 1 1 1 1 1
+
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print(6-i,end=" ")
+#     print("\r")
+
+# 9
+# 7 7
+# 5 5 5
+# 3 3 3 3
+# 1 1 1 1 1
+
+# a = 9
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print(a,end=" ")
+#     a = a - 2
+#     print("\r")
+
+# 2
+# 4 4
+# 6 6 6
+# 8 8 8 8
+
+# a = 2
+# for i in range(1,5):
+#     for j in range(0,i):
+#         print(a,end=" ")
+#     a = a + 2
+#     print("\r")
+
+# 8
+# 6 6
+# 4 4 4
+# 2 2 2 2
+
+# a = 8
+# for i in range(1,5):
+#     for j in range(0,i):
+#         print(a,end=" ")
+#     a = a - 2
+#     print("\r")
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print("*",end=" ")
+#     print("\r")

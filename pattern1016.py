7d8f388763a87104bfc0a453249b17594a0f5682
Ravi Kumawat <145572488+1998rv@users.noreply.github.com>
2023-10-18 10:54:15 +0530

    Add files via upload

--- pattern1016.py
+++ pattern1016.py
@@ -1,88 +1,59 @@
-# 1
-# 2 2
-# 3 3 3
-# 4 4 4 4
-# 5 5 5 5 5
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
 
 # for i in range(1,6):
 #     for j in range(0,i):
-#         print(i,end=" ")
+#         print("",end=" ")
+#     for l in range(6-i):
+#         print("*",end="")
 #     print("\r")
 
-# 5
-# 4 4
-# 3 3 3
-# 2 2 2 2
-# 1 1 1 1 1
-
+# 9 7 5 3 1
+#   7 5 3 1
+#     5 3 1
+#       3 1
+#         1
+#
+# a = 10
 # for i in range(1,6):
 #     for j in range(0,i):
-#         print(6-i,end=" ")
-#     print("\r")
-
-# 9
-# 7 7
-# 5 5 5
-# 3 3 3 3
-# 1 1 1 1 1
-
-# a = 9
-# for i in range(1,6):
-#     for j in range(0,i):
-#         print(a,end=" ")
+#         print("",end=" ")
+#     for l in reversed(range(1,a,2)):
+#         print(l,end="")
 #     a = a - 2
 #     print("\r")
 
-# 2
-# 4 4
-# 6 6 6
-# 8 8 8 8
-
-# a = 2
-# for i in range(1,5):
-#     for j in range(0,i):
-#         print(a,end=" ")
-#     a = a + 2
-#     print("\r")
-
-# 8
-# 6 6
-# 4 4 4
-# 2 2 2 2
+# 8 8 8 8 8
+#   6 6 6 6
+#     4 4 4
+#       2 2
+#         0
 
 # a = 8
-# for i in range(1,5):
+# for i in range(1,6):
 #     for j in range(0,i):
-#         print(a,end=" ")
+#         print("",end=" ")
+#     for l in range(6-i):
+#         print(a,end="")
 #     a = a - 2
 #     print("\r")
 
-# *
-# * *
-# * * *
-# * * * *
-# * * * * *
+# A A A A A
+#   B B B B
+#     C C C
+#       D D
+#         F
 
+
+# a = chr(65)
 # for i in range(1,6):
 #     for j in range(0,i):
-#         print("*",end=" ")
+#         print("",end=" ")
+#     for l in range(6-i):
+#         print(a,end="")
+#     a = chr(65+i)
 #     print("\r")
 
-#
-# car = {
-# "brand": "Ford",
-# "model": "Mustang",
-# "year": 1964
-# }
-#
-# car["color"] = "white"
-#
-# print(car) #after the change
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict["year"] = 2018
-print(thisdict)

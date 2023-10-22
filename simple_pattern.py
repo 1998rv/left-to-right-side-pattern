f1aa335c7c6c05a4202f2bad866c334178ad5129
Ravi Kumawat <145572488+1998rv@users.noreply.github.com>
2023-10-22 18:00:32 +0530

    #python #pycham #python_patten

---
+++ simple_pattern.py
@@ -0,0 +1,69 @@
+# Developer Name :- Ravi Kumawat
+# Date :- 20 -10-2023
+# Code :-  creat pattern
+# 1 1 1 1 1
+# 2 2 2 2 2
+# 3 3 3 3 3
+# 4 4 4 4 4
+# 5 5 5 5 5
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print("\r")
+
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print("\r")
+# 5 5 5 5 5
+# 4 4 4 4 4
+# 3 3 3 3 3
+# 2 2 2 2 2
+# 1 1 1 1 1
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(6-i,end=" ")
+#     print("\r")
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(6-j,end=" ")
+#     print("\r")
+
+# 1 3 5 7 9
+# 1 3 5 7 9
+# 1 3 5 7 9
+# 1 3 5 7 9
+# 1 3 5 7 9
+
+# for i in range(1,6):
+#     for j in range(1,10,2):
+#         print(j,end=" ")
+#     print("\r")
+
+# 9 9 9 9 9
+# 7 7 7 7 7
+# 5 5 5 5 5
+# 3 3 3 3 3
+# 1 1 1 1 1
+
+# a = 9
+# for i in range(1,6):
+#     for j in range(1,6):
+#       print(a,end=" ")
+#     a = a - 2
+#     print("\r")
+
